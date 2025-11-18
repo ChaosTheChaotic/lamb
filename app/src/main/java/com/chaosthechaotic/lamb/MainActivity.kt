ff6586fa068dd126ad0e5260954ff97b5b810a68
ChaosTheChaotic <james.moriaty.ot@gmail.com>
2025-11-18 18:16:09 +0000

    Initial commit

---
+++ app/src/main/java/com/chaosthechaotic/lamb/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.chaosthechaotic.lamb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.chaosthechaotic.lamb.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Example of a call to a native method
+        binding.sampleText.text = stringFromJNI()
+    }
+
+    /**
+     * A native method that is implemented by the 'lamb' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+        // Used to load the 'lamb' library on application startup.
+        init {
+            System.loadLibrary("lamb")
+        }
+    }
+}

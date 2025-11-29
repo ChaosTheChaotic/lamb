09e08da37772e5dcee142bc8991a76dca794c3b1
ChaosTheChaotic <james.moriaty.ot@gmail.com>
2025-11-29 23:51:39 +0000

    Rename file

--- app/src/main/java/com/chaosthechaotic/lamb/LambUIElements.kt
+++ app/src/main/java/com/chaosthechaotic/lamb/LambUIElements.kt
@@ -2,19 +2,27 @@ package com.chaosthechaotic.lamb
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
-class UIElements {
+class LambUIElements {
     @Composable
     fun SettingsButton(onClickAction: () -> Unit) {
         Box(
             modifier = Modifier.fillMaxSize(),
             contentAlignment = Alignment.Center
         ) {
-            IconButton(onClick = onClickAction) { }
+            IconButton(onClick = onClickAction) {
+                Icon(
+                    imageVector = Icons.Default.Settings,
+                    contentDescription = "Settings menu",
+                )
+            }
         }
     }
 }

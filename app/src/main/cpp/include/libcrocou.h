d6d9cba642174bea2943ee1b3222a12d7511ea6a
ChaosTheChaotic <james.moriaty.ot@gmail.com>
2025-12-02 22:36:06 +0000

    Use less-deprecated-header

--- app/src/main/cpp/include/libcrocou.h
+++ app/src/main/cpp/include/libcrocou.h
@@ -1,7 +1,7 @@
 #ifndef UNIFIED_LIBCROCO
 #define UNIFIED_LIBCROCO
 
-#include <stdint.h>
+#include <cstdint>
 #if INTPTR_MAX == INT32_MAX
     #include "libcroco_32.h"
 #elif INTPTR_MAX == INT64_MAX
